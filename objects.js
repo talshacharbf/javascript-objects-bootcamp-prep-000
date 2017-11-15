78af11b2cf67cbd96b410d2ad04116d2262a6a1f
tal shachar <tal.shachar@buzzfeed.com>
2017-11-15 01:12:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object({ artistName: 'songTitle' })
+var playlist = new Object({ })
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
